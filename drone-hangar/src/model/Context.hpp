fc2a91ed63c2fb1cd59f382b1d8574d2c1d1df60
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-12-03 14:05:41 +0100

    added kernel and started context

---
+++ drone-hangar/src/model/Context.hpp
@@ -0,0 +1,15 @@
+#ifndef __CONTEXT__
+#define __CONTEXT__
+
+class Context
+{
+    private:
+        bool droneIn;
+        bool droneOut;
+        bool landing;
+        bool takingOff;
+        bool alarm;
+        bool preAlarm;        
+};
+
+#endif 
