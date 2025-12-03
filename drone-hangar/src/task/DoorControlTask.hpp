54304497545a2abfe984f3b494498f53b00610cd
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2025-12-03 14:53:50 +0100

    added things to DoorControlTask

--- drone-hangar/src/task/DoorControlTask.hpp
+++ drone-hangar/src/task/DoorControlTask.hpp
@@ -4,11 +4,25 @@
 #include "kernel/Task.hpp"
 #include "model/Context.hpp"
 #include "devices/ServoMotor.hpp"
+#include <Arduino.h>
 
 class DoorControlTask : public Task {
     private:
         Context* context;
         ServoMotor* doorMotor;
+
+        void setState(int state);
+        long elapsedTimeInState();
+        void log(const String& msg);
+        
+        bool checkAndSetJustEntered();
+        
+        enum { IDLE, STARTING, SWEEPING_FWD, SWEEPING_BWD, RESETTING } state;
+        long stateTimestamp;
+        bool justEntered;
+
+        int currentPos;
+        bool toBeStopped;
 
     public:
         DoorControlTask(Context* ctx, ServoMotor* motor){
