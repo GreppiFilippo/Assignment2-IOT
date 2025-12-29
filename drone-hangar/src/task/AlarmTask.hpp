b132e5f4c61a81eee8e3f64e14fa61e0001e1758
Filippo <filippo.greppi2@studio.unibo.it>
2025-12-29 17:09:07 +0100

    Implement AlarmTask class with state management and logging functionality

--- drone-hangar/src/task/AlarmTask.hpp
+++ drone-hangar/src/task/AlarmTask.hpp
@@ -3,17 +3,27 @@
 
 #include "kernel/Task.hpp"
 #include "model/Context.hpp"
-#include <Arduino.h>
+#include <Arduino.hpp>
+#include "devices/TempSensorTMP36.hpp"
 
 class AlarmTask: public Task {
     private:
         Context* pContext;
+        TempSensorTMP36* tempSensor;
+        long stateTimestamp;
+        bool justEntered;
+        
+        enum { NORMAL, WAITING_PREALARM, PREALARM, WAITING_ALARM, ALARM } state;
 
+        void setState(int state);
+        long elapsedTimeInState();
+        void log(const String& msg);
+        bool checkAndSetJustEntered();
+        
     public:
-        AlarmTask(Context* pContext);
+        AlarmTask(TempSensorTMP36* tempSensor, Context* pContext);
         void init(int period);
         void tick();
-
-}
+};
 
 #endif /* __ALARM_TASK__ */
