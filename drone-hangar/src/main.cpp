7d08f60e07d7bffb9882bca814a613c7479bb8aa
Filippo <filippo.greppi2@studio.unibo.it>
2025-12-29 17:08:50 +0100

    Refactor main.cpp to improve structure and organization of setup function

--- drone-hangar/src/main.cpp
+++ drone-hangar/src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include "config.hpp"
+#include "kernel/Scheduler.hpp"
+#include "kernel/Logger.hpp"
+#include "kernel/Task.hpp"
+#include "kernel/MsgService.hpp"
+#include "model/HWPlatform.hpp"
+#include "model/Context.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+#define BASE_PERIOD_MS 50
+
+// Uncomment the following line to enable hardware testing mode
+// #define __TESTING_HW__
+
+Scheduler sched;
+HWPlatform* pHWPlatform;
+Context* pContext;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  MsgService.iinit();
+  sched.init(BASE_PERIOD_MS);
+
+  Logger.log(":::::: Drone Hangar ::::::");
+  
+  pHWPlatform = new HWPlatform();
+  pHWPlatform->init();
+
+#ifndef __TESTING_HW__
+  pContext = new Context();
+  
+  // Tasks
+  // TODO: add tasks here
+#endif
+
+#ifdef __TESTING_HW__
+  /* Testing */
+  Task* pTestHWTask = new TestHWTask(pHWPlatform);
+  pTestHWTask->init(2000);
+  sched.addTask(pTestHWTask);
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  sched.schedule();
 }
