42db2e9ef35464f04745f16e5e4fd8325f8097ea
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-12-30 16:21:41 +0100

    Remove unused VSCode tasks plugin from build.gradle.kts

--- drone-remote-unit/app/build.gradle.kts
+++ drone-remote-unit/app/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
     application
     id("com.gradleup.shadow") version "9.2.2"
     id("org.danilopianini.gradle-java-qa") version "1.155.0"
-    id("ru.akman.vscode-tasks") version "0.4.0"
 }
 
 repositories {
