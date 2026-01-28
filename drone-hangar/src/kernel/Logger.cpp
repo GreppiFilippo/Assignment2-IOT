1cbdcc50c609641c443e54898baa22821148f587
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2026-01-28 10:21:27 +0100

    Fixes doc

--- drone-hangar/src/kernel/Logger.cpp
+++ drone-hangar/src/kernel/Logger.cpp
@@ -4,10 +4,8 @@
 
 void LoggerService::log(const String& msg)
 {
-    // Invece di creare una nuova String "lo:" + msg,
-    // usiamo una funzione dedicata che non alloca memoria extra
-    MsgService.sendMsgRaw("lo:", false);       // Stampa senza newline
-    MsgService.sendMsgRaw(msg.c_str(), true);  // Stampa il contenuto e va a capo
+    MsgService.sendMsgRaw("lo:", false);
+    MsgService.sendMsgRaw(msg.c_str(), true);
 }
 
 void LoggerService::log(const __FlashStringHelper* msg)
--- drone-hangar/src/kernel/Logger.hpp
+++ drone-hangar/src/kernel/Logger.hpp
@@ -5,11 +5,27 @@
 
 /**
  * @brief Service for logging messages.
+ *
  */
 class LoggerService
 {
    public:
+    /**
+     * @brief Log a message.
+     *
+     * The message is prefixed with "lo:" to indicate it's a log entry.
+     *
+     * @param msg The message to log as a String.
+     */
     void log(const String& msg);
+
+    /**
+     * @brief Log a message.
+     *
+     * The message is prefixed with "lo:" to indicate it's a log entry.
+     *
+     * @param msg The message to log as a Flash string.
+     */
     void log(const __FlashStringHelper* msg);
 };
 extern LoggerService Logger;
